5bb33469684530adeda1f5bd946b1b7ec62c528a
DaTrederVid <dedemacmactretre@gmail.com>
2022-09-26 15:35:48 -0400

    Remove debug printing

--- gedcom_interp.py
+++ gedcom_interp.py
@@ -11,26 +11,17 @@ def isValidTag(tag):
 
 def formatLine(s):
     '''Returns list in format [level, tag, arguments]'''
-    print("formatting")
     l = s.split()
     if (len(l) < 2):
         return [0, 'ERROR', '']
     level = int(l[0])
     tag = l[1]
     args = ' '.join(l[2:])
-    print(level)
-    print(tag)
-    print(args)
     if level == 0 and (args == "FAM" or args == "INDI"):
-        print("special case")
         return [level, args, tag] #edge cases for INDI and FAM mean that the third element in s is the tag
     return [level, tag, args]
 
 def addElement(entry, elem, level):
-    #todo add special cases
-    print("Adding element")
-    print(entry)
-    print(elem)
     if (level[2] == "DATE"):
         s = level[1] + " " + level[2]
         entry[s] = elem[2]
@@ -132,42 +123,32 @@ def main():
     indiFlag = False
     famFlag = False
     with open(filename, 'r') as f:
-   
         for line in f:
             print(f'--> {line}', end='')
             l = formatLine(line)
             valid = 'Y' if isValidTag(l[1]) else 'N'
             if isValidTag(l[1]):
                 
-                #if tag is valid, check if INDI or FAM to save the information
                 if (l[0] == 0):
                     level = [l[1], None, None]
                 if (l[0] == 1):
                     level = [level[0], l[1], None]
                 if (l[0] == 2):
                     level = [level[0], level[1], l[1]]
-                print("--")
-                print(l)
-                print(level)
+
                 if (indiFlag or famFlag) and l[0] == 0:
-                    print(2)
                     #if we reach the end of an individual or family, add it to the list
                     if (indiFlag):
                         indi.append(curr)
                     else:
                         fam.append(curr)
                 if ((l[1] == 'INDI' or l[1] == 'FAM') and l[0] == 0):
-                    print(1)
                     curr = defaultIndi() if l[1] == 'INDI' else defaultFam()
                     addElement(curr, l, level)
-                    print("curr")
-                    print(curr)
                 if (l[0] == 0):
-                    print(3)
                     indiFlag = True if l[1] == 'INDI' else False
                     famFlag = True if l[1] == 'FAM' else False
                 if indiFlag or famFlag:
-                    print(4)
                     addElement(curr, l, level)
                 
 
@@ -176,15 +157,8 @@ def main():
         indi = [makeIndiAssumptions(i) for i in indi]
         fam = [makeFamAssumptions(f, indi) for f in fam]
 
-        # indi = [calculateAge(i) for i in indi]
-        print(len(indi))
-        print(indi)
-        print(len(fam))
-        print(fam)
-        print(dictListToPrettyTable(indi))
-        print(dictListToPrettyTable(fam))
-        for i in indi:
-            print(len(i))
+        print(dictListToPrettyTable(sorted(indi, key = lambda x: x["INDI"])))
+        print(dictListToPrettyTable(sorted(fam, key = lambda x: x["FAM"])))
 
 if __name__ == "__main__":
     main()
