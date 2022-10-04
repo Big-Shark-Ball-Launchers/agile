d0b5426d01f77a2ef0c9db803f0e941e13029b75
Tyler-Seliber <tylerseliber@gmail.com>
2022-10-03 22:12:55 -0400

    Enhancements
    Fixed bug where "DIV" was unhandled, removed extra outputs from previous assignments

--- gedcom_interp.py
+++ gedcom_interp.py
@@ -1,16 +1,18 @@
-#https://github.com/Big-Shark-Ball-Launchers/agile
-#David Treder, Tyler Seliber, Andrew Capro
-#I pledge my honor that I have abided by the Stevens Honor System
+# https://github.com/Big-Shark-Ball-Launchers/agile
+# David Treder, Tyler Seliber, Andrew Capro
+# I pledge my honor that I have abided by the Stevens Honor System
 
 import sys
 import datetime
 from user_stories import stories
 
+
 def isValidTag(tag):
-    validTags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", 
-         "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", 
-         "DATE", "HEAD", "TRLR", "NOTE"]
+    validTags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC",
+                 "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV",
+                 "DATE", "HEAD", "TRLR", "NOTE"]
     return tag in validTags
+
 
 def formatLine(s):
     '''Returns list in format [level, tag, arguments]'''
@@ -21,8 +23,10 @@ def formatLine(s):
     tag = l[1]
     args = ' '.join(l[2:])
     if level == 0 and (args == "FAM" or args == "INDI"):
-        return [level, args, tag] #edge cases for INDI and FAM mean that the third element in s is the tag
+        # edge cases for INDI and FAM mean that the third element in s is the tag
+        return [level, args, tag]
     return [level, tag, args]
+
 
 def addElement(entry, elem, level):
     key = ""
@@ -30,9 +34,9 @@ def addElement(entry, elem, level):
     if (level[2] == "DATE" and (level[1] == "BIRT" or level[1] == "DEAT" or level[1] == "MARR" or level[1] == "DIV")):
         key = level[1] + " " + level[2]
         value = elem[2]
-    elif (elem[1] == "BIRT" or elem[1] == "MARR"):
+    elif (elem[1] == "BIRT" or elem[1] == "MARR" or elem[1] == "DIV" or elem[1] == "DEAT"):
         return
-    elif (elem[1]=="CHIL" or elem[1]=="FAMS" or elem[1]=="FAMC"):
+    elif (elem[1] == "CHIL" or elem[1] == "FAMS" or elem[1] == "FAMC"):
         if not elem[1] in entry:
             key = elem[1]
             value = [elem[2]]
@@ -46,10 +50,12 @@ def addElement(entry, elem, level):
     if key != "":
         entry[key] = value
 
+
 def makeIndiAssumptions(indi):
     '''Fills in assumptions about the data.'''
     indi["AGE"] = calculateAge(indi)
     return indi
+
 
 def makeFamAssumptions(fam, indi):
     '''Fills in assumptions about the data.'''
@@ -57,20 +63,24 @@ def makeFamAssumptions(fam, indi):
     fam["WIFE NAME"] = indiIDtoName(indi, fam["WIFE"])
     return fam
 
+
 def gedStringToDatetime(s):
     '''Converts a gedcom date string to a datetime object'''
     try:
         return datetime.datetime.strptime(s, '%d %b %Y')
     except ValueError:
-        return datetime.datetime(1,1,1) #default in case of error
+        return datetime.datetime(1, 1, 1)  # default in case of error
+
 
 def datetimeToString(d):
     '''Converts a datetime object to a string in the format of a gedcom date'''
     return d.strftime('%d %b %Y').upper()
 
+
 def timedeltaToYears(d):
     '''converts a timedelta object to years'''
     return int(d.days/365.25)
+
 
 def calculateAge(indi):
     '''returns age of individual based on birth and death dates. If no death date, use current time.'''
@@ -81,6 +91,7 @@ def calculateAge(indi):
     return timedeltaToYears(end - gedStringToDatetime(indi["BIRT DATE"]))
     # return indi
 
+
 def dictListToPrettyTable(d):
     '''converts a list of dictionarys to PrettyTable'''
     from prettytable import PrettyTable
@@ -89,12 +100,14 @@ def dictListToPrettyTable(d):
         t.add_row(i.values())
     return t
 
+
 def indiIDtoName(indi, id):
     '''returns name of individual based on their id'''
     for i in indi:
         if i["INDI"] == id:
             return i["NAME"]
     return "NA"
+
 
 def defaultIndi():
     '''returns a default individual dictionary'''
@@ -110,6 +123,7 @@ def defaultIndi():
         "FAMS": []
     }
 
+
 def defaultFam():
     '''returns a default family dictionary'''
     return {
@@ -123,15 +137,18 @@ def defaultFam():
         "CHIL": []
     }
 
+
 def findIndi(iId, list):
     for i in list:
         if (i["INDI"] == iId):
             return i
 
+
 def findFam(fId, list):
     for f in list:
         if (f["FAM"] == fId):
             return f
+
 
 def displayAnomaly(storyKey, **kwargs):
     '''prints a formatted error/anomaly message'''
@@ -139,25 +156,26 @@ def displayAnomaly(storyKey, **kwargs):
     formattedAnaomalyString = anomalyString.format(**kwargs, story=storyKey)
     print(formattedAnaomalyString)
 
+
 def main():
     if (len(sys.argv) <= 1):
-        filename = 'My-Family-25-Sep-2022-221241779.ged' #default file path
+        filename = 'full_family.ged'  # default file path
     else:
         filename = sys.argv[1]
 
     indi = []
     fam = []
-    #keeps track of the tag of levels
+    # keeps track of the tag of levels
     level = []
     indiFlag = False
     famFlag = False
     with open(filename, 'r') as f:
         for line in f:
-            print(f'--> {line}', end='')
+            # print(f'--> {line}', end='')
             l = formatLine(line)
             valid = 'Y' if isValidTag(l[1]) else 'N'
             if isValidTag(l[1]):
-                
+
                 if (l[0] == 0):
                     level = [l[1], None, None]
                 if (l[0] == 1):
@@ -166,7 +184,7 @@ def main():
                     level = [level[0], level[1], l[1]]
 
                 if (indiFlag or famFlag) and l[0] == 0:
-                    #if we reach the end of an individual or family, add it to the list
+                    # if we reach the end of an individual or family, add it to the list
                     if (indiFlag):
                         indi.append(curr)
                     else:
@@ -179,14 +197,12 @@ def main():
                     famFlag = True if l[1] == 'FAM' else False
                 if indiFlag or famFlag:
                     addElement(curr, l, level)
-                
 
-
-            print(f'<-- {l[0]}|{l[1]}|{valid}|{l[2]}')
+            # print(f'<-- {l[0]}|{l[1]}|{valid}|{l[2]}')
         indi = [makeIndiAssumptions(i) for i in indi]
         fam = [makeFamAssumptions(f, indi) for f in fam]
-        print(dictListToPrettyTable(sorted(indi, key = lambda x: x["INDI"])))
-        print(dictListToPrettyTable(sorted(fam, key = lambda x: x["FAM"])))
+        print(dictListToPrettyTable(sorted(indi, key=lambda x: x["INDI"])))
+        print(dictListToPrettyTable(sorted(fam, key=lambda x: x["FAM"])))
 
         # Loop through each individual and family to check for errors/anomalies
         for i in indi:
@@ -194,50 +210,59 @@ def main():
             # US01
             currentDate = datetime.datetime.now()
             if (i["BIRT DATE"] != "NA" and gedStringToDatetime(i["BIRT DATE"]) > currentDate):
-                displayAnomaly("US01", id=i["INDI"], date=i["BIRT DATE"], dateType="BIRT", currentDate=datetimeToString(currentDate))
+                displayAnomaly("US01", id=i["INDI"], date=i["BIRT DATE"],
+                               dateType="BIRT", currentDate=datetimeToString(currentDate))
             if (i["DEAT DATE"] != "NA" and gedStringToDatetime(i["DEAT DATE"]) > currentDate):
-                displayAnomaly("US01", id=i["INDI"], date=i["DEAT DATE"], dateType="DEAT", currentDate=datetimeToString(currentDate))
-                
+                displayAnomaly("US01", id=i["INDI"], date=i["DEAT DATE"],
+                               dateType="DEAT", currentDate=datetimeToString(currentDate))
+
             # US03
-            if(i["AGE"] < 0):
-                displayAnomaly("US03", id = i["INDI"], dDate = i["DEAT DATE"], bDate = i["BIRT DATE"])
+            if (i["AGE"] < 0):
+                displayAnomaly(
+                    "US03", id=i["INDI"], dDate=i["DEAT DATE"], bDate=i["BIRT DATE"])
 
         for f in fam:
-        
+
             # US01
             if (f["MARR DATE"] != "NA" and gedStringToDatetime(f["MARR DATE"]) > currentDate):
-                displayAnomaly("US01", id=f["FAM"], date=f["MARR DATE"], dateType="MARR", currentDate=datetimeToString(currentDate))
+                displayAnomaly("US01", id=f["FAM"], date=f["MARR DATE"],
+                               dateType="MARR", currentDate=datetimeToString(currentDate))
             if (f["DIV DATE"] != "NA" and gedStringToDatetime(f["DIV DATE"]) > currentDate):
-                displayAnomaly("US01", id=f["FAM"], date=f["DIV DATE"], dateType="DIV", currentDate=datetimeToString(currentDate))
-                
+                displayAnomaly("US01", id=f["FAM"], date=f["DIV DATE"],
+                               dateType="DIV", currentDate=datetimeToString(currentDate))
+
             # US02
             husb = f["HUSB"]
             wife = f["WIFE"]
             husbIndi = findIndi(husb, indi)
             wifeIndi = findIndi(wife, indi)
-            if(husbIndi and timedeltaToYears(gedStringToDatetime(f["MARR DATE"]) - gedStringToDatetime(husbIndi["BIRT DATE"])) < 0):
-                displayAnomaly("US02", id = husbIndi["INDI"], mDate = f["MARR DATE"], bDate = husbIndi["BIRT DATE"])
-            if(wifeIndi and timedeltaToYears(gedStringToDatetime(f["MARR DATE"]) - gedStringToDatetime(wifeIndi["BIRT DATE"])) < 0):
-                displayAnomaly("US02", id = wifeIndi["INDI"], mDate = f["MARR DATE"], bDate = wifeIndi["BIRT DATE"])
-            
+            if (husbIndi and timedeltaToYears(gedStringToDatetime(f["MARR DATE"]) - gedStringToDatetime(husbIndi["BIRT DATE"])) < 0):
+                displayAnomaly(
+                    "US02", id=husbIndi["INDI"], mDate=f["MARR DATE"], bDate=husbIndi["BIRT DATE"])
+            if (wifeIndi and timedeltaToYears(gedStringToDatetime(f["MARR DATE"]) - gedStringToDatetime(wifeIndi["BIRT DATE"])) < 0):
+                displayAnomaly(
+                    "US02", id=wifeIndi["INDI"], mDate=f["MARR DATE"], bDate=wifeIndi["BIRT DATE"])
+
             # US04
-            if(f["DIV DATE"] != "NA"):
-                if(timedeltaToYears(gedStringToDatetime(f["DIV DATE"]) - gedStringToDatetime(f["MARR DATE"])) < 0):
-                    displayAnomaly("US04", id = f["FAM"], mDate = f["MARR DATE"], dDate = f["DIV DATE"])
+            if (f["DIV DATE"] != "NA"):
+                if (timedeltaToYears(gedStringToDatetime(f["DIV DATE"]) - gedStringToDatetime(f["MARR DATE"])) < 0):
+                    displayAnomaly(
+                        "US04", id=f["FAM"], mDate=f["MARR DATE"], dDate=f["DIV DATE"])
 
             # US05 Marriage before death
             marDate = gedStringToDatetime(f["MARR DATE"])
             husb = findIndi(f["HUSB"], indi)
             wife = findIndi(f["WIFE"], indi)
             if (husb and wife and marDate != "NA" and ((husb["DEAT DATE"] != "NA" and marDate > gedStringToDatetime(husb["DEAT DATE"])) or (wife["DEAT DATE"] != "NA" and marDate > gedStringToDatetime(wife["DEAT DATE"])))):
-                displayAnomaly('US05', id=f["FAM"], mDate = f["MARR DATE"])
+                displayAnomaly('US05', id=f["FAM"], mDate=f["MARR DATE"])
 
             # US06 Divorce before death
             divDate = gedStringToDatetime(f["DIV DATE"])
             husb = findIndi(f["HUSB"], indi)
             wife = findIndi(f["WIFE"], indi)
             if (husb and wife and divDate != "NA" and ((husb["DEAT DATE"] != "NA" and divDate > gedStringToDatetime(husb["DEAT DATE"])) or (wife["DEAT DATE"] != "NA" and divDate > gedStringToDatetime(wife["DEAT DATE"])))):
-                displayAnomaly('US06', id=f["FAM"], dDate = f["DIV DATE"])
+                displayAnomaly('US06', id=f["FAM"], dDate=f["DIV DATE"])
+
 
 if __name__ == "__main__":
     main()
