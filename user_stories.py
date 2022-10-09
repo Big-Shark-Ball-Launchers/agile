613c29b4c59b0858f7d5a0b846d2a82fc1841b6b
Tyler-Seliber <tylerseliber@gmail.com>
2022-10-09 19:29:48 -0400

    US09 code

--- user_stories.py
+++ user_stories.py
@@ -7,7 +7,7 @@ stories = {
     "US06": "ERROR: {story}: FAM {id}: Divorce date {dDate} occurs before death of one or both spouses",
     "US07": "ANOMALY: {story}: INDI {id}: Age over 150 years old: {age}",
     "US08": "ANOMALY: {story}: INDI {id}: Birth date {bDate} occurs before parents' marriage date {mDate} or more than 9 months after parents' divorce date {dDate} in FAM {famID}",
-    "US09": "",
+    "US09": "ANOMALY: {story}: INDI {id}: Birth date {bDate} occurs after mother's death {mDeath} or more than 9 months after father's death {dDeath}",
     "US10": "ANOMALY: {story}: INDI {id}: FAM {fam}: Marriage date: {date} occurs before age of 14",
     "US11": "ANOMALY: {story}: INDI {id} is an active spouse in muliple families. FAMS: {fams}",
     "US12": "ANOMALY: {story}: INDI {id}: Birthdate {bDate} occurs more than 60 years after mother's birthday: {mbDate} or more than 80 years after father's birthday: {fbDate}",
